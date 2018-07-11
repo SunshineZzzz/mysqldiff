392e69d0f2c9b181491dd653095b20abf12ae682
Sunshine'Z <zhangyang@pixelgame.net>
2018-07-11 19:53:27 +0800

    增加使用说明

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mysqldiff
-比较两个数据库中的所有表, 生成对应的ALTER语句
+mysqldiff比较两个数据库中所有表，生成对应的"alter"语句，需要注意：不支持外键
+确保安装php5.0以上的版本，编译mysqli
+
+使用方法:
+	php mysqldiff.php --help
+
--- mysqldiff.php
+++ mysqldiff.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * mysqldiff
- * mysqldiff比较两个数据库中所有表，生成对应的"alter"语句，需要注意：不支持外键
- * 使用方法:
- *			php mysqldiff.php --help
- * 确保安装php5.0以上的版本，编译mysqli
- */
-
 error_reporting(E_ALL);
 
 class MysqlDiff {
